0be8771d072b6b1aa2db6c40d77cdb9b7381d908
gaorui <2295463072@qq.com>
2020-11-29 23:20:27 +0800

    upgrade-week01-1129 : [update main.py]

--- week01/main.py
+++ week01/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 
-#from week01_log import log_func
-import week01_log
+from week01_log import log_func
+#import week01_log
 
 if __name__ == '__main__':
-    week01_log.log_func()
+    log_func()
 
